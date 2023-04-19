f3d2105cbf1a1116f7626f331fa91293eba03fc7
Pavan <p.dande@zensar.com>
2023-04-19 12:07:30 +0530

    bank application fund transfer

---
+++ TransferFunds/src/main/java/com/zensar/TransferFundsApplication.java
@@ -0,0 +1,13 @@
+package com.zensar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransferFundsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransferFundsApplication.class, args);
+	}
+
+}
